76ebb4d168c71a0ffc879da794c940cd15886fa9
Tyson Bailey <onaclov2000+github@gmail.com>
2014-07-23 14:40:08 -0600

    Update openlibrary.js

--- openlibrary.js
+++ openlibrary.js
@@ -1,1 +1,21 @@
-sdfasdg
+ $scope.addBook = function(e) 
+  {
+    $scope.method = 'GET';
+    $scope.url = "http://openlibrary.org/search.json?isbn=";
+    
+    $http({method: $scope.method, url: $scope.url + $scope.isbn}).
+      success(function(data, status)
+      {
+        $scope.status = status;
+        $scope.data = data;
+        if (data["docs"] != "")
+        {
+          $scope.books.$add({"title":data["docs"][0]["title"], isbn : $scope.isbn});
+        }
+      }).
+      error(function(data, status) 
+      {
+        $scope.data = data;
+        $scope.status = status;
+      });
+  }
