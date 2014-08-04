4ae83a9602090a35d9de59b3d10474ad1132d780
Tyson Bailey <onaclov2000+github@gmail.com>
2014-08-04 06:38:02 -0600

    Update openlibrary.js

--- openlibrary.js
+++ openlibrary.js
@@ -2,21 +2,24 @@
 // This guy should be returning promises of when it successfully send/fails emails.
 //      License: MIT
 
-  angular.module('book', []).factory('$book', ['$http', function($http) {
-      // This should eventually become search.json.isbn or .title, or .query.
-      // This could theoretically get quite large.
-      // any pull requests are welcomed
-      _method = 'GET';
-      _url = "http://openlibrary.org/search";
+angular.module('book', []).factory('$book', ['$http',
+    function($http) {
+        // This should eventually become search.json.isbn or .title, or .query.
+        // This could theoretically get quite large.
+        // any pull requests are welcomed
+        _method = 'GET';
+        _url = "http://openlibrary.org/search";
 
-      return { $search : function (number)  {
-                                               $http({method: _method, url: _url + ".json?isbn=" + number}).
-                                                 success(function(data, status){console.log("Passed" + data); return data;}). 
-                                                 error(function(data, status) {console.log("Failed" + data); return data;}) 
-                                           }
-             };
-      }
-      ]);
+        return {
+            search: function(number) {
+                $http({
+                    method: _method,
+                    url: _url + ".json?isbn=" + number
+                });
+            }
+        };
+    }
+]);
 
 
 // From: https://openlibrary.org/dev/docs/api/search
